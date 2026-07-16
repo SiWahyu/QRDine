f9f9367337c719bcd462ca1aa481adee021a8b3c
Wahyu Firman <fwahyu839@gmail.com>
2026-07-16 21:15:58 +0700

    feat(ui): improve empty cart state

--- src/features/cart/components/EmptyCart.jsx
+++ src/features/cart/components/EmptyCart.jsx
@@ -1,5 +1,6 @@
-import { Frown, MoveRight } from "lucide-react";
+import { MoveRight } from "lucide-react";
 import { useNavigate } from "react-router";
+import emptryCart from "@/assets/empty-cart.png";
 
 const EmptyOrder = () => {
   const navigate = useNavigate();
@@ -7,17 +8,21 @@ const EmptyOrder = () => {
   const handleClick = () => {
     navigate("/menu");
   };
+
   return (
-    <div className="flex flex-col mt-[10vh] justify-center items-center gap-6">
+    <div className="flex flex-col items-center justify-center gap-6 mt-[10svh]">
       <div className="flex flex-col items-center justify-center">
-        <Frown className="size-10 text-neutral-500" />
-        <h2 className="text-3xl font-medium text-center text-neutral-500">
+        <img src={emptryCart} alt="empty cart" className="w-2/3" />
+        <h2 className="text-lg font-medium text-center">
           Kamu belum memesan apapun{" "}
         </h2>
+        <div className="mb-10 text-center">
+          Temukan menumu, pantau daftarnya di sini.
+        </div>
       </div>
       <button
         onClick={handleClick}
-        className="flex flex-row items-center justify-center gap-2 px-6 py-3 font-medium text-blue-600 transition-colors duration-300 border border-blue-600 rounded-lg active:text-white hover:text-white group hover:bg-blue-600 active:bg-blue-600"
+        className="flex flex-row items-center justify-center w-full gap-2 px-6 py-2 font-medium text-white transition-colors duration-300 bg-blue-600 border border-blue-600 rounded-lg hover:text-white group hover:bg-blue-700 active:bg-blue-700"
       >
         <span className="text-lg">Pesan Sekarang</span>
         <MoveRight className="transition-transform duration-200 group-hover:translate-x-1" />
